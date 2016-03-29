4a56f933a0c18c69d499aabb503782c1ea75ccce
Nobody109 <dominik.rosenberger@gmail.com>
2016-03-29 17:02:51 +0200

    Excluding more tests* files. Adding helper to publish easier.

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from setuptools import setup, find_packages
 
 
@@ -5,15 +8,21 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
 setup(name='google_bigquery',
-      version='0.12',
+      version='0.13',
       description='Wrapper for Google\'s Bigquery Python API.',
       keywords='bigquery google',
       url='http://github.com/Nobody109/google_bigquery',
       author='Dominik Rosenberger',
       author_email='dominik.rosenberger@gmail.com',
       license='MIT',
-      packages=find_packages(exclude=['tests']),
+      packages=find_packages(exclude=['tests', 'test*']),
       install_requires=[
           'google-api-python-client',
       ],
